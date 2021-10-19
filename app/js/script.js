1ae7b9db8534259a584a4c7c505865b2d5386b22
Andrew Le <s3820098@rmit.edu.vn>
2021-10-19 11:09:11 +0700

    14. Pop up menu when clicking hamburger icon

--- app/js/script.js
+++ app/js/script.js
@@ -1,23 +1,48 @@
 let hambg = document.getElementById("hambgbtn");
 let ovl = document.getElementById("ovl");
+let popup = document.getElementById("popupmenu");
+popup.style.display = "none";
 let hambgClicked = false;
+
 hambgbtn.addEventListener("click", function () {
   if (!this.classList.contains("open")) this.classList.add("open");
   else this.classList.remove("open");
 
   if (hambgClicked === false) {
+    // keep track of state and change state
     hambgClicked = true;
+
+    // add fade in effect for overlay
     ovl.classList.add("fade-in");
     ovl.classList.remove("fade-out");
     ovl.style.opacity = 1;
+
+    // prevent body from scrolling
     document.body.style.height = "100%";
-    document.body.style.overflow = "hidden";
+    document.body.style.overflowY = "hidden";
+
+    // show pop up menu
+    popup.classList.add("fade-in");
+    popup.classList.remove("fade-out");
+    popup.style.display = "block";
   } else {
+    // keep track of state and change state
     hambgClicked = false;
+
+    // // add fade out effect for overlay
     ovl.classList.add("fade-out");
     ovl.classList.remove("fade-in");
     ovl.style.opacity = 0;
+
+    // allow body scrolling
     document.body.style.height = "";
-    document.body.style.overflow = "";
+    document.body.style.overflowY = "";
+
+    // hide pop up menu
+    popup.classList.add("fade-out");
+    popup.classList.remove("fade-in");
+    setTimeout(() => {
+      popup.style.display = "none";
+    }, 200);
   }
 });

690ca31aef9d1efd216ba7f6a14c628f997c1d73
Andrew Le <s3820098@rmit.edu.vn>
2021-10-19 09:28:23 +0700

    12. Handle overlay behind that pop-up menu

--- app/js/script.js
+++ app/js/script.js
@@ -1,4 +1,10 @@
-const hambg = document.getElementsByClassName("header__menu");
-hambg[0].addEventListener("click", function () {
-    this.classList.toggle("open");
+let hambg = document.getElementById("hambgbtn");
+let ovl = document.getElementById("ovl");
+ovl.style.display = "none";
+hambgbtn.addEventListener("click", function () {
+  if (!this.classList.contains("open")) this.classList.add("open");
+  else this.classList.remove("open");
+
+  if (ovl.style.display == "none") ovl.style.display = "block";
+  else ovl.style.display = "none";
 });
